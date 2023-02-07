10d75a01990f1ba1e4eb44380fdd040984f56d14
Tayná <taynasg@ciandt.com>
2023-02-07 09:41:14 -0300

    Adicionando objetos na Classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -2,11 +2,16 @@ public class Pessoa {
     private String nomeDaPessoa;
     private String universidade;
 
+
     Pessoa(String nomeDaPessoa, String universidade) {
         this.nomeDaPessoa = nomeDaPessoa;
         this.universidade = universidade;
 
     }
+
+    Pessoa newton = new  Pessoa("Newton","Cambridge");
+    Pessoa einstein = new Pessoa("Einstein","Princeton");
+
 
     public String retonaDados() {
         return "Nome: " + nomeDaPessoa + " \n " +
